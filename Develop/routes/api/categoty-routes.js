c8653bf96a157e38e9c1f92611d190ac518cda41
Hfranz1 <87962035+Hfranz1@users.noreply.github.com>
2022-03-10 09:43:44 -0700

    routes

--- Develop/routes/api/categoty-routes.js
+++ Develop/routes/api/categoty-routes.js
@@ -1,28 +1,32 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model { }
+
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-    // find all categories
-    // be sure to include its associated Products
-});
+Category.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        category_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'category',
+    }
+);
 
-router.get('/:id', (req, res) => {
-    // find one category by its `id` value
-    // be sure to include its associated Products
-});
-
-router.post('/', (req, res) => {
-    // create a new category
-});
-
-router.put('/:id', (req, res) => {
-    // update a category by its `id` value
-});
-
-router.delete('/:id', (req, res) => {
-    // delete a category by its `id` value
-});
-
-module.exports = router;
+module.exports = Category;
